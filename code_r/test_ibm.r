4b0688549db2ebac7bda344ef7d1b6b432932015
Tamora James <tdjames1@sheffield.ac.uk>
2015-07-15 09:40:21 +0100

    Tidy up simulation tests

--- code_r/test_ibm.r
+++ code_r/test_ibm.r
@@ -12,36 +12,43 @@ plot(pG,type="l")
 ## Simulation with G allele only
 sim.out <- doSim(mParFixEf, sim.length=500, init.pop.size=init.pop, init.G = "GG")
 simRunSum <- summariseSimRun(sim.out)
-plot(simRunSum$ntT, type="l", ylim=c(0,600))
+plot(simRunSum$ntT, type="l", ylim=c(0,600), main="GG")
 lines(simRunSum$ntF, col="red")
 lines(simRunSum$ntM, col="blue")
 
 ## Simulation with T allele only
 sim.out <- doSim(mParFixEf, sim.length=500, init.pop.size=init.pop, init.G = "TT")
 simRunSum <- summariseSimRun(sim.out)
-plot(simRunSum$ntT, type="l", ylim=c(0,600))
+plot(simRunSum$ntT, type="l", ylim=c(0,600), main="TT")
 lines(simRunSum$ntF, col="red")
 lines(simRunSum$ntM, col="blue")
 
-## Simulate invasion of superior into inferior genotype
-numReps <- 10000
+## Repeated invasion simulation of superior into inferior genotype to see if
+## results are consistent. Need to improve speed of code to make this complete
+## in a sensible time.
+numReps <- 1000
 resG <- "TT"
 mPar <- list(C=mParFixEf, S=mParStore)
-invProp <- c(0.01, 0.02, 0.05)
+invProp <- c(0.02, 0.05) # Density of invader as proportion of existing population
 invRate <- list()
 
 for (m in seq_along(mPar)) {
     for (i in invProp) {
+        simTitle <- paste(switch(names(mPar)[[m]], C="Constant", S="Stochastic"),
+                          "environment, proportion of invader:", i)
+        print(simTitle)
+
         label <- paste0(i,names(mPar)[[m]])
         invRate[[label]] <- simInv(mPar[[m]], inv.prop=i, num.reps=numReps, res=resG)
-
-        print(paste(switch(names(mPar)[[m]], C="Constant", S="Stochastic"),
-                    "environment, proportion of invader:", i))
 
         ## Mean invader log growth rate
         print(summary(invRate[[label]], na.rm=TRUE))
 
         ## Proportion positive vs negative growth rate
-        print(paste("Invasion rate positive", sum(invRate[[label]]>0)/numReps*100, "%"))
+        print(paste("Invasion rate positive", sum(invRate[[label]]>0, na.rm=TRUE)/numReps*100, "%"))
+
+        hist(invRate[[label]], breaks="scott", main=simTitle)
     }
 }
+
+save(invRate, file="ibmSimData.rda")
